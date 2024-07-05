8211e463ea80137afbdcc15a4736a2b7df3c9392
AC Capehart <github@accapehart.com>
2024-07-05 15:08:08 -0400

    refactor to keep original ps and my modified versions

---
+++ problemsets_with_solutions/1/mortgage_ps1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 14:02:59 2024
+
+@author: capehart
+"""
+
+
+def calculate_monthly_interest(current_savings, interest_rate):
+    return current_savings * interest_rate / 12
+
+# Assumptions:
+interest_rate = 0.04
+current_savings = 0
+portion_down_payment = 0.25
+
+# Input collection
+salary = int(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal (eg '.10'): "))
+total_cost = int(input("Enter the cost of your dream home: "))
+savings_required = total_cost * portion_down_payment
+
+months_elapsed = 0
+
+while current_savings <= savings_required:
+    months_elapsed += 1
+    current_savings += calculate_monthly_interest(current_savings=current_savings,
+                                                  interest_rate=interest_rate)
+    current_savings += salary/12 * portion_saved
+
+(years,months) = divmod(months_elapsed, 12)
+print(f"After {years} years and {months} months ({months_elapsed} total months), you will have saved ${current_savings:,.2f} which is enough to cover a ${savings_required:,.2f} down payment.")
+    
